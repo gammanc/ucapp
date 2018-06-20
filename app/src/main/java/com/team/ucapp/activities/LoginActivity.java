bdbdd67493afd8d1236d4f6ee1b6c7945ff64f5a
00010216 <00010216@uca.edu.sv>
2018-06-19 21:42:59 -0600

    se creo actividad login aun no contiene funcionalidades

--- app/src/main/java/com/team/ucapp/activities/LoginActivity.java
+++ app/src/main/java/com/team/ucapp/activities/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.team.ucapp;
+package com.team.ucapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.team.ucapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
