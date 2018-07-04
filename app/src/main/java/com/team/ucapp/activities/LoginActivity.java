8242d7c7aea529ac5bb7d34aebe87defe72d41fc
00010216 <00010216@uca.edu.sv>
2018-07-04 11:50:36 -0600

    se reconstruyo el activity

--- app/src/main/java/com/team/ucapp/activities/LoginActivity.java
+++ app/src/main/java/com/team/ucapp/activities/LoginActivity.java
@@ -1,4 +1,16 @@
 package com.team.ucapp.activities;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.team.ucapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
