47366118cd17649ac58ba75ffc6c88acbcc45544
00010216 <00010216@uca.edu.sv>
2018-07-04 11:45:13 -0600

    se hizo merge con master, y se agrego la version version en ingles de lo que estaba en  master

--- app/src/main/java/com/team/ucapp/activities/LoginActivity.java
+++ app/src/main/java/com/team/ucapp/activities/LoginActivity.java
@@ -1,15 +1,4 @@
 package com.team.ucapp.activities;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import com.team.ucapp.R;
-
-public class LoginActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+public class LoginActivity {
 }
---
+++ app/src/main/java/com/team/ucapp/ui/expedient/SubjectExpedient.java
@@ -0,0 +1,4 @@
+package com.team.ucapp.ui.expedient;
+
+public class SubjectExpedient {
+}
