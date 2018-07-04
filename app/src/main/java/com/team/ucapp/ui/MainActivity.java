623b5d6549af29a30510367ec986ad907b224fe2
00010216 <00010216@uca.edu.sv>
2018-07-04 11:48:18 -0600

    se hizo merge con master

--- app/src/main/java/com/team/ucapp/ui/MainActivity.java
+++ app/src/main/java/com/team/ucapp/ui/MainActivity.java
@@ -3,14 +3,19 @@ package com.team.ucapp.ui;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.team.ucapp.R;
+import com.team.ucapp.ui.expedient.ExpedientFragment;
 
 public class MainActivity extends AppCompatActivity
         implements BottomNavigationViewEx.OnNavigationItemSelectedListener{
@@ -32,6 +37,13 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
     private void findViews(){
         bottomnavigationView = findViewById(R.id.bottom_nav);
         bottomnavigationView.enableAnimation(false);
@@ -47,18 +59,29 @@ public class MainActivity extends AppCompatActivity
         //Fragment selectedFragment = null;
         switch (item.getItemId()){
             case R.id.item_home:
+                //selectedFragment = new ExpedientFragment();
                 break;
             case R.id.item_calendar:
                 break;
             case R.id.item_grades:
                 break;
         }
-        /**if (selectedFragment!=null){
+        /*if (selectedFragment!=null){
          FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
          transaction.replace(R.id.main_content, selectedFragment);
          transaction.commit();
          return true;
-         }**/
+         }*/
         return true;
+    }
+
+
+    public void startFragment(String title, Fragment frag){
+        setTitle(title);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.main_content, frag);
+        fragmentTransaction.commit();
     }
 }
