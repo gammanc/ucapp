fd0d97af1b63a959babbbfac380d5cd7127a5c1e
00010216 <00010216@uca.edu.sv>
2018-07-04 11:55:45 -0600

    se creo adapter para manejar las materias dentro del expediente

--- app/src/main/java/com/team/ucapp/ui/expedient/SubjectExpedient.java
+++ app/src/main/java/com/team/ucapp/ui/expedient/SubjectExpedient.java
@@ -1,4 +1,37 @@
 package com.team.ucapp.ui.expedient;
 
 public class SubjectExpedient {
+    private String subjectLetter;
+    private String subject;
+    private String grade;
+
+    public String getSubjectLetter() {
+        return subjectLetter;
+    }
+
+    public SubjectExpedient(String subjectLetter, String subject, String grade) {
+        this.subjectLetter = subjectLetter;
+        this.subject = subject;
+        this.grade = grade;
+    }
+
+    public void setSubjectLetter(String subjectLetter) {
+        this.subjectLetter = subjectLetter;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
 }
