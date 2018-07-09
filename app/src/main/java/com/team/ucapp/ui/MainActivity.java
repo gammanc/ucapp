77e0c47fe093544813abf8e52d45501ad2f451ea
00010216 <00010216@uca.edu.sv>
2018-07-08 23:03:03 -0600

    se hizo cambios en los item y se cambio el them del Main Activity

--- app/src/main/java/com/team/ucapp/ui/MainActivity.java
+++ app/src/main/java/com/team/ucapp/ui/MainActivity.java
@@ -43,8 +43,8 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
         findViews();
 
         fragmentManager = getSupportFragmentManager();
