fda6725d4c9909176d6631d67d9a9c99b7c2c3e6
kevin <0010216@uca.edu.sv>
2018-07-10 01:49:41 -0600

    Notificación desde Firebase implementada

---
+++ app/src/main/java/com/team/ucapp/utils/MiFirebaseInstanceIdService.java
@@ -0,0 +1,21 @@
+package com.team.ucapp.utils;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MiFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    public static final String TAG = "NOTICIAS";
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+
+        String token  = FirebaseInstanceId.getInstance().getToken();
+
+        Log.d(TAG, "Token: "+ token);
+    }
+
+}
